6fa90a94a0484b2f7730ba6f4b4473d7f78ee2f7
ScottyOBrien <obrie023@rangers.uwp.edu>
2019-04-06 12:17:40 -0500

    Added buffered reader/getting points from user input file.

--- src/ClosestPoints.java
+++ src/ClosestPoints.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 /**
  * @author Scott O'Brien
  * @version 3/20/2019
@@ -6,4 +9,31 @@
  */
 
 public class ClosestPoints {
+
+    public static void main(String[] args) {
+
+        //Get the file name from the user. Using scanner.
+        Scanner inputFile = new Scanner(System.in);
+        System.out.print("Please enter file name with extension (e.g. \"text.dat\"): ");
+        String fileName = inputFile.nextLine();
+
+        //File file;
+        //Grab data from file using buffered reader, we'll adjust this later to put the points into their respective
+        //arrays.
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String points = reader.readLine();
+
+            while (points != null) {
+                System.out.println(points);
+                points = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
